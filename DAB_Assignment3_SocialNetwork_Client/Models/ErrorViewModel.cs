7615f52e9a8b3043d3bb80ad6b1d1caeb72678af
TristanMoe <Trilleo@hotmail.com>
2019-05-12 12:07:21 +0200

    Added Client project

---
+++ DAB_Assignment3_SocialNetwork_Client/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DAB_Assignment3_SocialNetwork_Client.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
