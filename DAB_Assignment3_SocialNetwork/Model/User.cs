0052b0409102fa9c5cf2e82fbf6090e0c66e9a36
Marcus Gasberg <mrgasberg@hotmail.com>
2019-05-12 12:25:11 +0200

    Sln and user update

--- DAB_Assignment3_SocialNetwork/Model/User.cs
+++ DAB_Assignment3_SocialNetwork/Model/User.cs
@@ -2,10 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
-namespace DAB_Assignment3_SocialNetwork.Model
+namespace SocialNetwork.Server.Model
 {
     public class User
     {
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("FirstName")]
+        public string FirstName { get; set; }
+        [BsonElement("LastName")]
+        public string LastName { get; set; }
+
+        [BsonElement("Email")]
+        public string Email { get; set; }
+
+        [BsonElement("Gender")]
+        public string Gender { get; set; }
     }
 }
--- DAB_Assignment3_SocialNetwork/Program.cs
+++ DAB_Assignment3_SocialNetwork/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace DAB_Assignment3_SocialNetwork
+namespace SocialNetwork.Server
 {
     public class Program
     {
