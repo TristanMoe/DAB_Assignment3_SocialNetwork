b75868c4fd6d04fb6ff37ba553b4d368b80f757c
TristanMoe <Trilleo@hotmail.com>
2019-05-13 11:18:11 +0200

    Added some more javascrpit in usercookie

--- DAB_Assignment3_SocialNetwork_Client/wwwroot/js/UserCookie.js
+++ DAB_Assignment3_SocialNetwork_Client/wwwroot/js/UserCookie.js
@@ -1,6 +1,6 @@
-﻿function getCookie() {
+﻿var userId = -1; 
+function getCookie() {
     let email = decodeURIComponent(document.cookie);
-
     return email; 
 }
 
@@ -13,4 +13,23 @@ function deleteCookies() {
         var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
         document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
     }
+}
+
+function saveUser(inputEmail, inputPassword) {
+    fetch('/api/UserController/', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ Email: inputEmail, Password: inputPassword}),
+        })
+        .then(response => response.json()) // response.json() returns a promise
+        .then((response) => {
+            user = response; 
+            console.log("You saved this item", response); //returns the new item along with its ID
+        })
+}
+
+function getUser() {
+    return userId; 
 }
