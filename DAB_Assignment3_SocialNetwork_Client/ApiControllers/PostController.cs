09f9cb211c6c92d5b85e58e2e9b5cf296e96411e
Marcus Gasberg <mrgasberg@hotmail.com>
2019-05-13 11:32:50 +0200

    Merged the project because cors sucks

---
+++ DAB_Assignment3_SocialNetwork_Client/ApiControllers/PostController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using SocialNetwork.Model;
+using SocialNetwork.Services;
+
+namespace SocialNetwork.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        private PostServices _postServices;
+
+        public PostController(PostServices service)
+        {
+            _postServices = service;
+        }
+        // GET: api/Post
+        [HttpGet("{ids}")]
+        public ActionResult<List<Post>> GetPosts(List<string> ids)
+        {
+            return _postServices.GetPublicPosts(ids);
+        }
+
+
+        // GET: api/Post/5
+        [HttpGet("{id}", Name = "Get")]
+        public ActionResult<Post> GetPost(string id)
+        {
+            var post = _postServices.GetPublicPost(id);
+            if (post == null)
+                return BadRequest();
+            return post;
+
+        }
+
+        // POST: api/Post
+
+        [HttpPut("{id}")]       
+        public void UpdatePost(string id,[FromBody]Post post)
+        {
+           
+            _postServices.UpdatePost(id,post);
+            
+        }
+
+        // PUT: api/Post/5
+
+        
+        [HttpPost(Name="Create")]      
+        public void Create([FromBody]Post post)
+        {
+            _postServices.InsertPost(post);
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(string id)
+        {
+            _postServices.DeletePost(id);
+        }
+    }
+}
---
+++ DAB_Assignment3_SocialNetwork_Client/Model/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace SocialNetwork.Model
+{
+    public class Comment
+    {
+        [BsonDateTimeOptions(Kind = DateTimeKind.Local)]
+        public DateTime CommentTimeStamp { get; set; }
+
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string CommentAuthorUserId { get; set; }
+        public string Text { get; set; }
+    }
+}
