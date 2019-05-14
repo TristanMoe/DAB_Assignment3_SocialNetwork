2b5ac18868a37c5b7337b5d79329c359afe542b0
Marcus Gasberg <mrgasberg@hotmail.com>
2019-05-14 13:05:21 +0200

    Fixed some functions and made login functionality

--- DAB_Assignment3_SocialNetwork_Client/wwwroot/js/UserCookie.js
+++ DAB_Assignment3_SocialNetwork_Client/wwwroot/js/UserCookie.js
@@ -4,7 +4,7 @@
         apiUrl: "https://localhost:44375/api/"
     },
     setUser(newUser) {
-        console.log("user got set to", user);
+        console.log("user got set to", newUser);
         this.user = newUser;
     }
 }
@@ -26,16 +26,63 @@ function deleteCookies() {
 }
 
 function saveUser(inputEmail, inputPassword) {
-    fetch('https://localhost:44375/api/User/', {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ Email: inputEmail, Password: inputPassword}),
-        })
-        .then(response => response.json()) // response.json() returns a promise
-        .then((response) => {
-            user = response; 
-            console.log("You saved this item", response); //returns the new item along with its ID
-        })
+	fetch('https://localhost:44375/api/User/',
+			{
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json"
+				},
+				body: JSON.stringify({ Email: inputEmail, Password: inputPassword })
+			})
+		.then(response => response.json()) // response.json() returns a promise
+		.then((response) => {
+			applicationState.setUser(response);
+			console.log("You saved this item", response); //returns the new item along with its ID
+		});
+}
+
+function subscribeUser(userToSubscribeTo) {
+	applicationState.data.currentUser.put("subscriptionIds", userToSubscribeTo.userId);
+	userToSubscribeTo.put("subscriberIds", applicationState.data.currentUser.userId);
+	let updateUrl = applicationState.data.apiUrl + "User/" + userToSubscribeTo.userId;
+	fetch(updateUrl,
+			{
+				method: "PUT",
+				body: JSON.stringify(userToSubscribeTo),
+				headers: new Headers({ "Content-Type": "application/json" })
+			})
+		.catch(error => console.log("Error while updating user: ", error)
+			.then(response => console.log("Success: ", response)
+			));
+}
+
+
+function getAllUsers() {
+	let url = applicationState.data.apiUrl + "User/";
+	console.log(`Fetching: ${url} ...`);
+	return fetch(url)
+		.then(response => response.json())
+		.catch(err => console.log("Error while fetching all users:", err));
+}
+
+function getUserById(id) {
+	let url = `${applicationState.data.apiUrl}User/${id}`;
+	console.log(`Fetching: ${url} ...`);
+	return fetch(url)
+		.then((response) => response.json())
+		.catch(err => console.log("Error while fetching user:", err));
+}
+
+function getUserByCredentials(email, password) {
+	let url = `${applicationState.data.apiUrl}User/ByCredentials/${email}/${password}`;
+	console.log(`Fetching: ${url} ...`);
+	return fetch(url)
+		.then((response) => response.json())
+		.catch(err => console.log("Error while fetching user:", err));
+}
+
+function login(email, password) {
+	return getUserByCredentials(email,password)
+		.then((userToLogin) => applicationState.setUser(userToLogin))
+		.catch(err => `Error happened when logging in: ${err}`);
 }
